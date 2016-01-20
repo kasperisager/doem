5a67d22001f6834654954dbb3e275a2cc5ccd8fc
Kasper Isager <kasperisager@gmail.com>
2016-01-20 12:12:41 +0100

    Implement html()

---
+++ test/html.js
@@ -0,0 +1,29 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import html from '../lib/html';
+
+test('gets the inner HTML of an element', t => {
+  const scope = fragment(`
+    <div><span></span></div>
+  `);
+
+  const element = find(scope, 'div');
+
+  t.is(html(element), '<span></span>');
+  t.end();
+});
+
+test('sets the inner HTML of an element', t => {
+  const scope = fragment(`
+    <html>
+      <div><span></span></div>
+    </html>
+  `);
+
+  const element = find(scope, 'div');
+
+  html(element, '<a></a>');
+  t.is(html(element), '<a></a>');
+  t.end();
+});
