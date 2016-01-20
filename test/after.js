8e7498c5a9676c57c99b74c3decbf154cec5e455
Kasper Isager <kasperisager@gmail.com>
2016-01-20 12:41:35 +0100

    Implement before() and after()

---
+++ test/after.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import html from '../lib/html';
+import after from '../lib/after';
+
+test('inserts content after an element', t => {
+  const scope = fragment(`
+    <div><span>foo</span></div>
+  `);
+
+  const div = find(scope, 'div');
+  const span = find(scope, 'span');
+
+  after(span, 'bar');
+  t.is(html(div), '<span>foo</span>bar');
+  t.end();
+});
---
+++ test/before.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import html from '../lib/html';
+import before from '../lib/before';
+
+test('inserts content before an element', t => {
+  const scope = fragment(`
+    <div><span>foo</span></div>
+  `);
+
+  const div = find(scope, 'div');
+  const span = find(scope, 'span');
+
+  before(span, 'bar');
+  t.is(html(div), 'bar<span>foo</span>');
+  t.end();
+});
