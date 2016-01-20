f7b9f5ac3f90843f9cd630b06258f5585f6eff20
Kasper Isager <kasperisager@gmail.com>
2016-01-20 22:18:59 +0100

    Implement append() and prepend()

---
+++ test/append.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import html from '../lib/html';
+import append from '../lib/append';
+
+test('inserts content at the end of an element', t => {
+  const scope = fragment(`
+    <div><span>foo</span></div>
+  `);
+
+  const div = find(scope, 'div');
+  const span = find(scope, 'span');
+
+  append(span, 'bar');
+  t.is(html(div), '<span>foobar</span>');
+  t.end();
+});
---
+++ test/prepend.js
@@ -0,0 +1,18 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import html from '../lib/html';
+import prepend from '../lib/prepend';
+
+test('inserts content at the beginning of an element', t => {
+  const scope = fragment(`
+    <div><span>foo</span></div>
+  `);
+
+  const div = find(scope, 'div');
+  const span = find(scope, 'span');
+
+  prepend(span, 'bar');
+  t.is(html(div), '<span>barfoo</span>');
+  t.end();
+});
