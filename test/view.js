29a817c2ef54136ea6e7e90033f3be0517db88c1
Kasper Isager <kasperisager@gmail.com>
2016-01-20 22:01:06 +0100

    Implement view()

---
+++ test/view.js
@@ -0,0 +1,29 @@
+import test from 'tape';
+import frame from '../lib/frame';
+import find from '../lib/find';
+import view from '../lib/view';
+
+test('gets the window an element is contained within', t => {
+  const context = frame(`
+    <div></div>
+  `);
+
+  const {document: scope} = context;
+
+  const element = find(scope, 'div');
+
+  t.is(view(element), context);
+
+  context.close();
+  t.end();
+});
+
+test('gets the window a document is contained within', t => {
+  t.is(view(document), window);
+  t.end();
+});
+
+test('gets the window itself when given a window', t => {
+  t.is(view(window), window);
+  t.end();
+});
