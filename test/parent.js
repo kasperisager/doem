959c13add254babb8a0a6e60df542769a26e1ff6
Kasper Isager <kasperisager@gmail.com>
2016-01-20 22:47:26 +0100

    Implement parent()

---
+++ test/parent.js
@@ -0,0 +1,45 @@
+import test from 'tape';
+import fragment from '../lib/fragment';
+import find from '../lib/find';
+import parent from '../lib/parent';
+
+test('gets the parent of an element', t => {
+  const scope = fragment(`
+    <div id=foo>
+      <div id=bar></div>
+    </div>
+  `);
+
+  const bar = find(scope, '#bar');
+
+  t.is(parent(bar).id, 'foo');
+  t.end();
+});
+
+test('can filter by a selector', t => {
+  const scope = fragment(`
+    <div id=foo>
+      <div id=bar></div>
+    </div>
+  `);
+
+  const bar = find(scope, '#bar');
+
+  t.is(parent(bar, '#foo').id, 'foo');
+  t.end();
+});
+
+test('returns null if no match is found', t => {
+  const scope = fragment(`
+    <div id=foo>
+      <div id=bar></div>
+    </div>
+  `);
+
+  const foo = find(scope, '#foo');
+  const bar = find(scope, '#bar');
+
+  t.is(parent(foo), null);
+  t.is(parent(bar, '#bar'), null);
+  t.end();
+});
