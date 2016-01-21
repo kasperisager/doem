f25147a762108ddd86cc269b8e895ccf99754bd9
Kasper Isager <kasperisager@gmail.com>
2016-01-21 13:26:20 +0100

    Implement style()

---
+++ test/style.js
@@ -0,0 +1,27 @@
+import test from 'tape';
+import frame from '../lib/frame';
+import find from '../lib/find';
+import style from '../lib/style';
+
+test('gets the computed style of an element', t => {
+  const context = frame(`
+    <style>
+      div {
+        color: blue;
+      }
+    </style>
+
+    <div style="background-color: red"></div>
+  `);
+
+  const {document: scope} = context;
+
+  const element = find(scope, 'div');
+  const computed = style(element);
+
+  t.is(computed.color, 'rgb(0, 0, 255)');
+  t.is(computed['background-color'], 'rgb(255, 0, 0)');
+
+  context.close();
+  t.end();
+});
