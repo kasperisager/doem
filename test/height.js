e84a2a18325329b0b84532eb8155c102fe0f9a93
Kasper Isager <kasperisager@gmail.com>
2016-01-21 13:46:23 +0100

    Implement height() and width()

---
+++ test/height.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+import frame from '../lib/frame';
+import find from '../lib/find';
+import height from '../lib/height';
+
+test('gets the computed height of an element', t => {
+  const context = frame(`
+    <style>
+      div {
+        padding: 10px;
+        line-height: 20px;
+      }
+    </style>
+
+    <div>Foo</div>
+  `);
+
+  const {document: scope} = context;
+
+  const element = find(scope, 'div');
+
+  t.is(height(element), 40);
+  context.close();
+  t.end();
+});
+
+test('gets the computed height of a document', t => {
+  t.is(height(document), document.documentElement.scrollHeight);
+  t.end();
+});
+
+test('gets the computed height of a window', t => {
+  t.is(height(window), document.documentElement.clientHeight);
+  t.end();
+});
---
+++ test/width.js
@@ -0,0 +1,35 @@
+import test from 'tape';
+import frame from '../lib/frame';
+import find from '../lib/find';
+import width from '../lib/width';
+
+test('gets the computed width of an element', t => {
+  const context = frame(`
+    <style>
+      div {
+        width: 20px;
+        padding: 10px;
+      }
+    </style>
+
+    <div></div>
+  `);
+
+  const {document: scope} = context;
+
+  const element = find(scope, 'div');
+
+  t.is(width(element), 40);
+  context.close();
+  t.end();
+});
+
+test('gets the computed width of a document', t => {
+  t.is(width(document), document.documentElement.scrollWidth);
+  t.end();
+});
+
+test('gets the computed height of a window', t => {
+  t.is(width(window), document.documentElement.clientWidth);
+  t.end();
+});
